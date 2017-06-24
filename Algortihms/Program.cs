f5d319f63679c4874162a4652f444160fcd72c34
Ola Popp <olapopp85@gmail.com>
2017-06-24 19:38:51 +0200

    Add project files.

---
+++ Algortihms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algortihms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
